1cfb6fb2df61962ca130c970b822a7dd88df07ca
piwonski12 <bartoszpiwona@gmail.com>
2023-03-15 12:18:56 +0100

    -wstępna wersja fiszek dodana v1.1

--- lib/flashcards.dart
+++ lib/flashcards.dart
@@ -1,3 +1,4 @@
+import 'package:fiszki/main_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flip_card/flip_card.dart';
 
@@ -7,7 +8,12 @@ class Flashcards extends StatelessWidget {
     return MaterialApp(
       title: 'Fiszki',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lime,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+          foregroundColor: Colors.yellow,
+        )
       ),
       home: FlashcardsPage(),
     );
@@ -20,7 +26,72 @@ class FlashcardsPage extends StatefulWidget {
 }
 
 class _FlashcardsPageState extends State<FlashcardsPage> {
-  List<Flashcard> flashcards = [    Flashcard('Apple', 'Jabłko'),    Flashcard('Grape', 'Wino w wodzie'),    Flashcard('Pear', 'Gruszka'),  ];
+  List<Flashcard> flashcards = [
+    Flashcard('Animal', 'Zwierzę'),
+    Flashcard('Art', 'Sztuka'),
+    Flashcard('Baby', 'Dziecko'),
+    Flashcard('Backpack', 'Plecak'),
+    Flashcard('Ball', 'Piłka'),
+    Flashcard('Bathroom', 'Łazienka'),
+    Flashcard('Bed', 'Łóżko'),
+    Flashcard('Bicycle', 'Rower'),
+    Flashcard('Bird', 'Ptak'),
+    Flashcard('Book', 'Książka'),
+    Flashcard('Bottle', 'Butelka'),
+    Flashcard('Box', 'Pudełko'),
+    Flashcard('Boy', 'Chłopiec'),
+    Flashcard('Bread', 'Chleb'),
+    Flashcard('Breakfast', 'Śniadanie'),
+    Flashcard('Building', 'Budynek'),
+    Flashcard('Bus', 'Autobus'),
+    Flashcard('Business', 'Biznes'),
+    Flashcard('Butterfly', 'Motyl'),
+    Flashcard('Car', 'Samochód'),
+    Flashcard('Cat', 'Kot'),
+    Flashcard('Chair', 'Krzesło'),
+    Flashcard('City', 'Miasto'),
+    Flashcard('Clothes', 'Ubrania'),
+    Flashcard('Cloud', 'Chmura'),
+    Flashcard('Coffee', 'Kawa'),
+    Flashcard('Computer', 'Komputer'),
+    Flashcard('Cookie', 'Ciastko'),
+    Flashcard('Country', 'Kraj'),
+    Flashcard('Cousin', 'Kuzyn'),
+    Flashcard('Cow', 'Krowa'),
+    Flashcard('Cup', 'Kubek'),
+    Flashcard('Dad', 'Tata'),
+    Flashcard('Dance', 'Taniec'),
+    Flashcard('Daughter', 'Córka'),
+    Flashcard('Day', 'Dzień'),
+    Flashcard('Desk', 'Biurko'),
+    Flashcard('Dinner', 'Obiad'),
+    Flashcard('Dog', 'Pies'),
+    Flashcard('Door', 'Drzwi'),
+    Flashcard('Drawing', 'Rysunek'),
+    Flashcard('Dream', 'Sen'),
+    Flashcard('Dress', 'Sukienka'),
+    Flashcard('Drink', 'Napój'),
+    Flashcard('Ear', 'Ucho'),
+    Flashcard('Earth', 'Ziemia'),
+    Flashcard('Egg', 'Jajko'),
+    Flashcard('Electricity', 'Elektryczność'),
+    Flashcard('Elephant', 'Słoń'),
+    Flashcard('Elevator', 'Winda'),
+    Flashcard('Email', 'E-mail'),
+    Flashcard('Engine', 'Silnik'),
+    Flashcard('Evening', 'Wieczór'),
+    Flashcard('Eye', 'Oko'),
+    Flashcard('Face', 'Twarz'),
+    Flashcard('Family', 'Rodzina'),
+    Flashcard('Fan', 'Wiatrak'),
+    Flashcard('Farm', 'Farma'),
+    Flashcard('Father', 'Ojciec'),
+    Flashcard('Feet', 'Stopy'),
+    Flashcard('Film', 'Film'),
+    Flashcard('Finger', 'Palec'),
+    Flashcard('Fire', 'Ogień'),
+    Flashcard('Fish', 'Ryba'),
+  ];
 
   int index = 0;
 
@@ -46,32 +117,46 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Fiszki'),
+        leading: IconButton (
+          icon: Icon(Icons.arrow_back_ios_new),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Main()),
+              );
+            },
+        ),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           FlipCard(
-            direction: FlipDirection.HORIZONTAL, // kierunek, w którym ma być przesunięcie
-            front: Container(
-              height: 200,
-              width: 300,
-              color: Colors.blue,
-              child: Center(
-                child: Text(
-                  flashcards[index].pageone,
-                  style: TextStyle(fontSize: 36, color: Colors.white),
+            direction: FlipDirection.HORIZONTAL,
+            front: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // ustalenie zaokrąglenia rogów
+              child: Container(
+                height: 500,
+                width: 330,
+                color: Colors.white10,
+                child: Center(
+                  child: Text(
+                    flashcards[index].pageone,
+                    style: TextStyle(fontSize: 36, color: Colors.white),
+                  ),
                 ),
               ),
             ),
-            back: Container(
-              height: 200,
-              width: 300,
-              color: Colors.green,
-              child: Center(
-                child: Text(
-                  flashcards[index].pagetwo,
-                  style: TextStyle(fontSize: 36, color: Colors.white),
+            back: ClipRRect(
+              borderRadius: BorderRadius.circular(20), // ustalenie zaokrąglenia rogów
+              child: Container(
+                height: 500,
+                width: 330,
+                color: Colors.lime,
+                child: Center(
+                  child: Text(
+                    flashcards[index].pagetwo,
+                    style: TextStyle(fontSize: 36, color: Colors.black),
+                  ),
                 ),
               ),
             ),
@@ -80,14 +165,31 @@ class _FlashcardsPageState extends State<FlashcardsPage> {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              ElevatedButton(
-                onPressed: _prevFlashcard,
-                child: Text('Poprzednia'),
+              Container(
+                width: 125,
+                decoration: BoxDecoration(
+                  color: Colors.lime,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.arrow_back_ios_new),
+                  color: Colors.cyan,
+                  onPressed: _prevFlashcard,
+                ),
               ),
-              ElevatedButton(
-                onPressed: _nextFlashcard,
-                child: Text('Następna'),
+              Container(
+                width: 125,
+                decoration: BoxDecoration(
+                  color: Colors.lime,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: IconButton(
+                  icon: Icon(Icons.arrow_forward_ios),
+                  color: Colors.cyan,
+                  onPressed: _nextFlashcard,
+                ),
               ),
+
             ],
           ),
         ],
@@ -102,3 +204,4 @@ class Flashcard {
 
   Flashcard(this.pageone, this.pagetwo);
 }
+
