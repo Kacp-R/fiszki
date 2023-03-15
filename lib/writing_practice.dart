56bf5e6a6e6327bfd158e6192eb0d6fbeae97ad2
Gwarnick <nikita.gwardiak@gmail.com>
2023-03-15 12:20:39 +0100

    NO NO NIE DZIALA HEHEHE HA

--- lib/writing_practice.dart
+++ lib/writing_practice.dart
@@ -1,18 +1,77 @@
-//nikita
-
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-class WritingPrac extends StatelessWidget {
+class WritingPrac extends StatefulWidget {
+  @override
+  WritingPracState createState() => WritingPracState();
+}
+
+class WritingPracState extends State<WritingPrac> {
+  final List<String> english = [
+    'Apple', 'Banana', 'Cherry', 'Dog', 'Elephant', 'Football', 'Guitar', 'House', 'Ice cream', 'Jacket'
+  ];
+  final List<String> polish = [
+    'Jablko', 'Banan', 'Wisnia', 'Pies', 'Slon', 'Pilkanozna', 'Gitara', 'Dom', 'lody', 'kurtka'
+  ];
+  int lb = Random().nextInt(10);
+  String jen = "EN >>> PL";
+  String que = 'AMONGUS';
+  String odp = '', dz = "", podane = "";
+
+
+  void losuj(){
+      lb = Random().nextInt(10);
+      setState(() {
+        odp = english[lb];
+      });
+  }
+  void Sprawdz() {
+      if(podane == polish[lb]){
+          dz = "DOBRZE!!!";
+      }
+      else{
+          dz = "ZLE>:(";
+      }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(''),
-      ),
       body: Center(
-        child: Text(
-          'cos',
-          style: TextStyle(fontSize: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              jen,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Text(
+              que,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            Text(
+              odp,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'ODPOWIEDZ',
+                border: OutlineInputBorder(),
+              ),
+              onChanged: (text) {
+                setState(() {
+                  podane = text;
+                });
+              },
+            ),
+            Text(
+              dz,
+              style: TextStyle(fontSize: 24.0),
+            ),
+            ElevatedButton(
+              onPressed: Sprawdz,
+              child: Text('Sprawdz'),
+            ),
+          ],
         ),
       ),
     );
