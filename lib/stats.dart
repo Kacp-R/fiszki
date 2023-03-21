3f11249a4327c8c60714d1776a8c967bb4c7f795
Kacp-R <gryba.kacper@gmail.com>
2023-03-21 12:03:18 +0100

    Added a simple graph

--- lib/stats.dart
+++ lib/stats.dart
@@ -1,11 +1,22 @@
 //kacper
-
+import 'package:pie_chart/pie_chart.dart';
 import 'package:flutter/material.dart';
 
-class Stats extends StatelessWidget {
+class Stats extends StatefulWidget {
   final int cAnswers;
   final int tAnswers;
   Stats({required this.cAnswers, required this.tAnswers});
+  @override
+  StatsState createState() => StatsState(cAnswers, tAnswers);
+}
+
+class StatsState extends State<Stats> {
+  final int cAnswers;
+  final int tAnswers;
+  final Map<String, double> dataMap;
+
+  StatsState(this.cAnswers, this.tAnswers)
+  :dataMap = {'Poprawne': double.parse(cAnswers.toString()),'Niepoprawne': double.parse((tAnswers-cAnswers).toString())};
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +37,7 @@ class Stats extends StatelessWidget {
             '$tAnswers',
             style: TextStyle(fontSize: 24.0, color: Colors.white),
           ),
+          PieChart(dataMap: dataMap, chartRadius: 80, legendOptions: LegendOptions(legendTextStyle: TextStyle(color: Colors.white)),)
         ],
         ),
       ),
