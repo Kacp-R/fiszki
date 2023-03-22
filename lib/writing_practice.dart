6ce845ebce76388da4a28f891e996e6b9f9c886c
Gwarnick5 <nikita.gwardiak@gmail.com>
2023-03-22 12:36:33 +0100

    GIT

--- lib/writing_practice.dart
+++ lib/writing_practice.dart
@@ -39,20 +39,20 @@ class WritingPracState extends State<WritingPrac> {
     podane = "";
     jen = "";
     if(Random().nextBool() == true){
-      jen = "EN >>> PL";
+      jen = "EN --> PL";
     }
     else{
-      jen = "PL >>> EN";
+      jen = "PL --> EN";
     }
     losuj();
   }
   void losuj(){
       lb = Random().nextInt(10);
       setState(() {
-        if(jen == "EN >>> PL") {
+        if(jen == "EN --> PL") {
           odp = english[lb];
         }
-        else if(jen == "PL >>> EN"){
+        else if(jen == "PL --> EN"){
           odp = polish[lb];
         }
       });
@@ -60,7 +60,7 @@ class WritingPracState extends State<WritingPrac> {
   void sprawdz() {
     setState(() {
       que = "";
-      if(jen == "EN >>> PL"){
+      if(jen == "EN --> PL"){
         if(podane.toLowerCase() == polish[lb].toLowerCase()){
           dz = "DOBRZE!!!";
           streak++;
@@ -71,7 +71,7 @@ class WritingPracState extends State<WritingPrac> {
           streak = 0;
         }
       }
-      else if(jen == "PL >>> EN"){
+      else if(jen == "PL --> EN"){
         if(podane.toLowerCase() == english[lb].toLowerCase()){
           dz = "DOBRZE!!!";
           streak++;
@@ -114,86 +114,105 @@ class WritingPracState extends State<WritingPrac> {
       ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.start,
           children: <Widget>[
-            Text(
-              jen,
-              style: TextStyle(
+            Container(
+              margin: EdgeInsets.all(20),
+              child:Text(
+                jen,
+                style: TextStyle(
                   fontSize: 24.0,
                   color: Colors.white,
+                ),
               ),
             ),
-            Text(
-              odp,
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
-              ),
-            ),
-            TextField(
-              decoration: InputDecoration(
-                labelText: 'ODPOWIEDŹ',
-                labelStyle: TextStyle(
-                  fontSize: 30.0,
+            Container(
+              margin: EdgeInsets.all(20),
+              child:Text(
+                odp,
+                style: TextStyle(
+                  fontSize: 24.0,
                   color: Colors.white,
                 ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child:TextField(
+                decoration: InputDecoration(
+                  labelText: 'ODPOWIEDŹ',
+                  labelStyle: TextStyle(
+                    fontSize: 30.0,
                     color: Colors.white,
-                    width: 24.0,
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.white,
+                      width: 24.0,
+                    ),
+                  ),
+                ),
+                onChanged: (text) {
+                  setState(() {
+                    podane = text;
+                  });
+                },
+                controller: fieldText,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(20),
+              child:Text(
+                dz,
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child:Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Container(
+                  margin: EdgeInsets.all(10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      ElevatedButton(
+                        onPressed: sprawdz,
+                        child: Text('Sprawdz'),
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(Size(80, 40)),
+                          padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                        ),
+                      ),
+                      ElevatedButton(
+                        onPressed: nastepne,
+                        child: Text('Nastepne'),
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(Size(80, 40)),
+                          padding: MaterialStateProperty.all(EdgeInsets.all(10)),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
-              onChanged: (text) {
-                setState(() {
-                  podane = text;
-                });
-              },
-              controller: fieldText,
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
-              ),
             ),
-            Text(
-              dz,
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16.0),
-              child: Container(
-                margin: EdgeInsets.all(10),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    ElevatedButton(
-                      onPressed: sprawdz,
-                      child: Text('Sprawdz'),
-                      style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(80, 40)),
-                        padding: MaterialStateProperty.all(EdgeInsets.all(10)),
-                      ),
-                    ),
-                    ElevatedButton(
-                      onPressed: nastepne,
-                      child: Text('Nastepne'),
-                      style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(Size(80, 40)),
-                        padding: MaterialStateProperty.all(EdgeInsets.all(10)),
-                      ),
-                    ),
-                  ],
+            Container(
+              margin: EdgeInsets.all(10),
+              child:Text(
+                "streak: " + streak.toString(),
+                style: TextStyle(
+                  fontSize: 24.0,
+                  color: Colors.white,
                 ),
-              ),
-            ),
-            Text(
-              "streak: " + streak.toString(),
-              style: TextStyle(
-                fontSize: 24.0,
-                color: Colors.white,
               ),
             ),
           ],
